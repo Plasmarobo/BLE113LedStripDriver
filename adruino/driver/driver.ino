690aef37fb6e9341830d7c10ea48547ecc5093ad
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2015-12-29 21:30:58 -0500

    LE113 works

--- adruino/driver/driver.ino
+++ adruino/driver/driver.ino
@@ -1,39 +1,72 @@
 #include <SPI.h>
 
 const unsigned short MAX_LEDS = 480;
+const byte LEDS_PER_METER = 32;
 
+unsigned short num_leds;
+byte meters;
+
+byte strip_data[1440]; //(480*3)
+byte zero[] = {0,0,0,0,0}; //at most 5m
+unsigned short strip_pos;
+
+void setLedCount(unsigned short led_count)
+{
+  num_leds = (led_count < MAX_LEDS) ? led_count : MAX_LEDS;
+  meters = num_leds / LEDS_PER_METER;
+}
+
+void writeStripData()
+{
+  SPI.beginTransaction(SPISettings(300, MSBFIRST, SPI_MODE0));
+  SPI.transfer((void*)&(strip_data[0]), (num_leds*3));
+  writeZero();
+  SPI.endTransaction();
+  strip_pos = 0;
+}
+
+byte sinePattern(byte center, float period_ms, byte amplitude = 0)
+{
+  if(amplitude = 0)
+  {
+    amplitude = center;
+  }
+  return center + (amplitude * sin(2*PI*(((float)millis())/period_ms))); 
+}
 
 void setup() {
   SPI.begin();
-  SPI.transfer(
+  setLedCount(160); //Single strip for now
+  for(int i = 0; i < num_leds; ++i)
+  {
+    writeRGB(0,0,0);
+  }
+  writeStripData();
 }
 
 void loop() {
-  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-  writeRGB(20,0,0);
-  writeRGB(0,20,0);
-  writeRGB(0,0,20);
-  
-  SPI.endTransaction();
+  //Implement some sort of pattern
+  for(int i = 0; i < num_leds; ++i)
+  {
+    //Generate three sines between 0 and 128
+    writeRGB(12, 0,0);
+  }
+  writeStripData();
   delay(1000);
-  writeZero();
-  writeZero();
-  writeZero();
-  writeZero();
-  writeZero();
 }
 
 void writeRGB(byte r, byte g, byte b)
 {
-  SPI.transfer(g|0x80);
-  SPI.transfer(r|0x80);
-  SPI.transfer(b|0x80);
+  strip_data[strip_pos++] = g | 0x80;
+  strip_data[strip_pos++] = r | 0x80;
+  strip_data[strip_pos++] = b | 0x80;
 }
 
 void writeZero()
 {
-  SPI.transfer(0);
+  SPI.transfer( (void*)&(zero[0]), meters);
 }
+
 
 
 
