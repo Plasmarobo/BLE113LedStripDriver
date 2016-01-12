635b62410218f8b71a35f890e38b00790e361c8b
Plasmarobo <plasmarobo@gmail.com>
2016-01-11 21:50:45 -0500

    Basic app

---
+++ LEDCmdApp/mobile/src/main/java/ledcmd/plasmarobo/com/ledcommand/FindDevice.java
@@ -0,0 +1,125 @@
+package ledcmd.plasmarobo.com.ledcommand;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class FindDevice extends Activity {
+    public final int REQUEST_ENABLE_BT = 32;
+    BluetoothLeScanner le;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_device);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    protected void tryEnableBt()
+    {
+        BluetoothManager manager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter adapter = manager.getAdapter();
+        if (adapter == null)
+        {
+            Log.e("Adapter Acquisition Failed", "unknown error");
+            finish();
+            System.exit(1);
+
+        }
+        else
+        {
+            if (!adapter.isEnabled())
+            {
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            }
+            else
+            {
+                this.findDevice(adapter);
+            }
+        }
+    }
+
+    protected void findDevice(BluetoothAdapter adapter)
+    {
+        List<ScanFilter> filters = new ArrayList<>();
+        filters.add(new ScanFilter.Builder()
+                .setDeviceName("LED Strip")
+                .setServiceUuid(android.os.ParcelUuid.fromString("47f1de41-c535-414f-a747-1184246636c6"))
+                .build());
+        ScanSettings ss = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
+        le = adapter.getBluetoothLeScanner();
+
+        le.startScan(filters, ss, new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                Log.i("callbackType", String.valueOf(callbackType));
+                Log.i("result", result.toString());
+                BluetoothDevice btDevice = result.getDevice();
+                Intent i = new Intent(getApplicationContext(), LEDControl.class);
+                i.putExtra("BluetoothDevice", btDevice);
+                le.stopScan(this);
+                startActivity(i);
+            }
+
+            @Override
+            public void onBatchScanResults(List<ScanResult> results) {
+                for (ScanResult sr : results) {
+                    Log.i("ScanResult - Results", sr.toString());
+                }
+                onScanResult(0, results.get(0));
+            }
+
+            @Override
+            public void onScanFailed(int errorCode) {
+                Log.e("Scan Failed", "Error Code: " + errorCode);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int req, int res, Intent data)
+    {
+        if(req == REQUEST_ENABLE_BT)
+        {
+            if(res == Activity.RESULT_CANCELED)
+            {
+                Log.e("Adapter Acquisition Failed", "Permission denied");
+                finish();
+                System.exit(1);
+            }
+            tryEnableBt();
+        }
+    }
+
+
+
+}
--- LEDCmdApp/mobile/src/main/java/ledcmd/plasmarobo/com/ledcommand/LEDControl.java
+++ LEDCmdApp/mobile/src/main/java/ledcmd/plasmarobo/com/ledcommand/LEDControl.java
@@ -1,31 +1,148 @@
 package ledcmd.plasmarobo.com.ledcommand;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
-public class LEDControl extends AppCompatActivity {
+import java.util.UUID;
 
+public class LEDControl extends Activity {
+    private SeekBar red;
+    private SeekBar green;
+    private SeekBar blue;
+    private SeekBar.OnSeekBarChangeListener onColorChanged;
+    private ImageView preview;
+    private Button add;
+    private Button write;
+    private Button clear;
+    private Button set_led_count;
+    private EditText led_count;
+    private byte red_value;
+    private byte green_value;
+    private byte blue_value;
+    private String color_buffer;
+    private BluetoothDevice bt;
+    private BluetoothGatt gatt_service;
+    private BluetoothGattCharacteristic rgb_gatt;
+    private BluetoothGattCharacteristic cmd_gatt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ledcontrol);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        bt = null;
+        if(getIntent().getExtras() != null) {
+            bt = getIntent().getExtras().getParcelable("BluetoothDevice");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        }
+        if(bt == null)
+        {
+            finish();
+        }
+
+        gatt_service = bt.connectGatt(this, true, new BluetoothGattCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onCharacteristicWrite(gatt, characteristic, status);
+                Log.i("GATT WRITE", characteristic + " (" + status + ")");
             }
         });
+
+        rgb_gatt = new BluetoothGattCharacteristic(UUID.fromString("f408b6c7-06c0-4b4a-8493-50bc261ea9e7"),
+                BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE ,
+                BluetoothGattCharacteristic.PERMISSION_WRITE);
+        cmd_gatt = new BluetoothGattCharacteristic(UUID.fromString("f408b6c7-06c0-4b4a-8493-50bc261ea9e8"),
+                BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE,
+                BluetoothGattCharacteristic.PERMISSION_WRITE);
+
+        setContentView(R.layout.activity_ledcontrol);
+        red = (SeekBar)findViewById(R.id.red);
+        green = (SeekBar)findViewById(R.id.green);
+        blue = (SeekBar)findViewById(R.id.blue);
+        color_buffer = ""; red_value = 0; green_value = 0; blue_value = 0;
+        preview = (ImageView)findViewById(R.id.preview);
+        add = (Button)findViewById(R.id.add);
+        write = (Button)findViewById(R.id.write);
+        clear = (Button)findViewById(R.id.clear);
+        set_led_count = (Button)findViewById(R.id.set_count);
+        led_count = (EditText) findViewById(R.id.led_count);
+        this.onColorChanged = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                red_value = (byte)red.getProgress();
+                green_value = (byte)green.getProgress();
+                blue_value = (byte)blue.getProgress();
+                preview.setBackgroundColor(Color.rgb(red_value, green_value, blue_value));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+        red.setOnSeekBarChangeListener(this.onColorChanged);
+        green.setOnSeekBarChangeListener(this.onColorChanged);
+        blue.setOnSeekBarChangeListener(this.onColorChanged);
+        add.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                color_buffer += red_value;
+                color_buffer += green_value;
+                color_buffer += blue_value;
+            }
+        });
+        write.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Write massive string
+                rgb_gatt.setValue(color_buffer);
+                gatt_service.writeCharacteristic(rgb_gatt);
+
+                cmd_gatt.setValue(0, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+                gatt_service.writeCharacteristic(cmd_gatt);
+            }
+        });
+        clear.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cmd_gatt.setValue(2, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+                gatt_service.writeCharacteristic(cmd_gatt);
+            }
+        });
+        set_led_count.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int leds = Integer.parseInt(led_count.getText().toString());
+                cmd_gatt.setValue((short) leds, BluetoothGattCharacteristic.FORMAT_UINT16, 0);
+                gatt_service.writeCharacteristic(cmd_gatt);
+            }
+        });
+
     }
 
     @Override
@@ -49,4 +166,6 @@ public class LEDControl extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
